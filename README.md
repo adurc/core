7d2cf9a3fa54dc1e8616e7d19a218857f7d5a5a0
Bernat Vadell Puigros <bvadell@feimsoft.com>
2020-12-16 08:09:39 +0100

    fix npm lint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ADURC
+
+![Node.js Package](https://github.com/adurc/core/workflows/Node.js%20Package/badge.svg?branch=main)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Adurc } from "./adurc";
-import AdurcDriver from "./bedrock/driver";
-import AdurcIntrospector from "./bedrock/introspector";
-import { AdurcBuilder } from "./builder";
-import { AdurcContext } from "./interfaces/context";
-import { AdurcModel } from "./interfaces/model";
+import { Adurc } from './adurc';
+import AdurcDriver from './bedrock/driver';
+import AdurcIntrospector from './bedrock/introspector';
+import { AdurcBuilder } from './builder';
+import { AdurcContext } from './interfaces/context';
+import { AdurcModel } from './interfaces/model';
 
 export {
     Adurc,
