0a487d034b01e6ab7fcb065dddf05c27bdba39d9
Bernat Vadell Puigros <bvadell@feimsoft.com>
2020-12-20 13:21:32 +0100

    remove id as a type

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,8 +1,8 @@
 export type AdurcPrimitive = string | number | boolean;
 export type AdurcValue = AdurcPrimitive | AdurcObject<unknown> | AdurcValue[];
 export type AdurcObject<T = unknown> = Record<string, T>;
-export type AdurcPrimitiveDefinition = 'id' | 'string' | 'int' | 'uuid' | 'float' | 'boolean' | 'date' | 'buffer';
+export type AdurcPrimitiveDefinition = 'string' | 'int' | 'uuid' | 'float' | 'boolean' | 'date' | 'buffer';
 
 export function isAdurcPrimitiveType(type: string): boolean {
-    return ['id', 'string', 'int', 'uuid', 'float', 'boolean', 'date', 'buffer'].indexOf(type) >= 0;
+    return [string', 'int', 'uuid', 'float', 'boolean', 'date', 'buffer'].indexOf(type) >= 0;
 }
