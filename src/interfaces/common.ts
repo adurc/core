1e2970801e20dde733773561161e960eb7e377ed
Bernat Vadell Puigros <bvadell@feimsoft.com>
2020-12-20 12:53:34 +0100

    add helper function to check if type is primitive

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -2,3 +2,7 @@ export type AdurcPrimitive = string | number | boolean;
 export type AdurcValue = AdurcPrimitive | AdurcObject<unknown> | AdurcValue[];
 export type AdurcObject<T = unknown> = Record<string, T>;
 export type AdurcPrimitiveDefinition = 'id' | 'string' | 'int' | 'uuid' | 'float' | 'boolean' | 'date' | 'buffer';
+
+export function isAdurcPrimitiveType(type: string): boolean {
+    return ['id', 'string', 'int', 'uuid', 'float', 'boolean', 'date', 'buffer'].indexOf(type) >= 0;
+}
