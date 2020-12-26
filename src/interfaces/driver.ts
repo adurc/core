c068a1df1e55a2ecc85751ede17c75d738c6f94e
Bernat Vadell Puigros <bvadell@feimsoft.com>
2020-12-26 12:19:41 +0100

    add model as argument in driver methods

--- src/interfaces/driver.ts
+++ src/interfaces/driver.ts
@@ -4,11 +4,12 @@ import { AdurcCreateArgs } from './client/create.args';
 import { AdurcDeleteArgs } from './client/delete.args';
 import { AdurcFindManyArgs } from './client/find-many.args';
 import { AdurcUpdateArgs } from './client/update';
+import { AdurcModel } from './model';
 
 export interface AdurcDriver {
-    createMany(args: AdurcCreateArgs): Promise<BatchResult> | BatchResult;
-    findMany(args: AdurcFindManyArgs): Promise<unknown[]> | unknown[];
-    updateMany(args: AdurcUpdateArgs): Promise<BatchResult> | BatchResult;
-    deleteMany(args: AdurcDeleteArgs): Promise<BatchResult> | BatchResult;
-    aggregate(args: AdurcAggregateArgs): Promise<unknown> | unknown;
+    createMany(model: AdurcModel, args: AdurcCreateArgs): Promise<BatchResult> | BatchResult;
+    findMany(model: AdurcModel, args: AdurcFindManyArgs): Promise<unknown[]> | unknown[];
+    updateMany(model: AdurcModel, args: AdurcUpdateArgs): Promise<BatchResult> | BatchResult;
+    deleteMany(model: AdurcModel, args: AdurcDeleteArgs): Promise<BatchResult> | BatchResult;
+    aggregate(model: AdurcModel, args: AdurcAggregateArgs): Promise<unknown> | unknown;
 }
